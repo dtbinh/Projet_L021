0be0869c3821964250e42bc39b0c6977a45e5a87
NicolasSzewe <nicolas.szewe@gmail.com>
2014-06-11 15:49:50 +0200

    Premier pas interface graphique

---
+++ Projet/Acceuil.cpp
@@ -0,0 +1,14 @@
+﻿#include "Acceuil.h"
+#include "ui_acceuil.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
